2d9980bf97cd52a9f5855992c78d0b31951c5d2b
Saor <saor@ezspeak.eu>
2018-12-24 15:59:08 +0000

    map vars

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,20 @@
-var APIKey = "AIzaSyAASdEbImyKmxzYvC1Dl672n0doZmc5GPQ";
-var restrict = "key=API_KEY--AIzaSyAvUKXxRGX3q5SeRGinCXnna0ALPxcEa0g";
-
-
 // Initialize and add the map
 function initMap() {
   var labels = "ABCDEFGH";
+  var Munich = {lat: 48.13, lng: 11.58};
+  var Ulm = {lat: 48.4, lng: 10.0};
+  var Bergamo = {lat: 45.69, lng: 9.67};
+  var Madrid = {lat: 40.41, lng: -3.7};
+  var Malaga = {lat: 37.72, lng: -4.42};
+  var London = {lat: 51.5, lng: 0.0};
+  var Dublin = {lat: 53.34, lng: -6.3};
+  var Angers = {lat: 47.5, lng: -0.5};
+  
   var locations = [
-      {lat: 46.24, lng: 10.936},
-      {lat: 52.344, lng: -9.73},
-      {lat: 36.344, lng: -4.036},
-      {lat: 47.84, lng: 12.046}
+      Munich, Ulm, Bergamo, Madrid, Malaga, London, Dublin, Angers
       ];
   // The location of Uluru
-  var uluru = {lat: 46.344, lng: 11.036};
+  var uluru = {lat: 46.344, lng: 7.036};
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
   // The marker, positioned at Uluru
