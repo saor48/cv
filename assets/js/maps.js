bae0495778bfcb92976f5841e164fcab0967a7d7
Saor <saor@ezspeak.eu>
2018-12-27 19:03:57 +0000

    added 2 project links for github page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -14,7 +14,7 @@ function initMap() {
       Munich, Ulm, Bergamo, Madrid, Malaga, London, Dublin, Angers
       ];
   // The location of Uluru
-  var uluru = {lat: 46.344, lng: 7.036};
+  var uluru = {lat: 46.344, lng: 5.036};
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});
   // The marker, positioned at Uluru
